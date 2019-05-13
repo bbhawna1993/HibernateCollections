bf404c79ada4779b8f0031a624d6ce1a2f34895f
bhawna budhiraja <bhawna.bhawna3@soprasteria.com>
2019-05-13 12:12:11 +0530

    changed name of tables by using @JoinTable annotations

--- src/main/java/com/io/hibernatemaven/UserDetails.java
+++ src/main/java/com/io/hibernatemaven/UserDetails.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 
-@Entity
+@Entity(name="USER_DETAILS")
 public class UserDetails {
 
 	@Id @GeneratedValue
@@ -16,6 +17,7 @@ public class UserDetails {
 	private String userName;
 
 	@ElementCollection
+	@JoinTable(name="USER_ADDRESS",joinColumns=@JoinColumn(name="USER_ID"))
 	private List<Address> userAddress=new ArrayList<Address>();
 
 	public int getUserId() {
